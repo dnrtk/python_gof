c64e42084b85a76ae39b595c0cf2958d04b6f9f9
dnrtk <asaz.dnrtk@gmail.com>
2024-05-03 21:31:29 +0900

    singleton追加

---
+++ 01_Singleton.py
@@ -0,0 +1,16 @@
+class Singleton:
+    _instance = None
+    
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
+if __name__ == "__main__":
+    singleton = Singleton()
+    print(singleton)
+    
+    singleton2 = Singleton()
+    print(singleton2)
+
+    assert singleton is singleton2
